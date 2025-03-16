801e43af065b478678d19e63a16aabe0f5161b81
laksith19 <admin@laksith.dev>
2025-03-15 20:58:20 -0700

    no ssh files etc...

--- home.nix
+++ home.nix
@@ -1,44 +1,38 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  home.username = "laksith";
-  home.homeDirectory = "/home/l/la/laksith";
+  home = {
+    username = "laksith";
+    homeDirectory = "/home/l/la/laksith";
 
-  home.packages = with pkgs; [
-    lsd
-    ripgrep
-    jetbrains.idea-community-bin
-  ];
+    packages = with pkgs; [
+      lsd
+      ripgrep
+      jetbrains.idea-community-bin
+    ];
 
-  programs = {
-    home-manager.enable = true;
-    bash.enable = true;
-    starship.enable = true;
-    git = {
-      enable = true;
-      userName = "laksith19";
-      userEmail = "admin@laksith.dev";
+    programs = {
+      home-manager.enable = true;
+      bash.enable = true;
+      starship.enable = true;
+      git = {
+        enable = true;
+        userName = "laksith19";
+        userEmail = "admin@laksith.dev";
 
-      extraConfig = {
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedsignersfile = "${toString config.home.homeDirectory}/.ssh/allowed_signers";
-        user.signingkey = "${toString config.home.homeDirectory}/.ssh/id_ed25519.pub";
-        init.defaultbranch = "main";
+        extraConfig = {
+          commit.gpgsign = true;
+          gpg.format = "ssh";
+          gpg.ssh.allowedsignersfile = "${toString config.home.homeDirectory}/.ssh/allowed_signers";
+          user.signingkey = "${toString config.home.homeDirectory}/.ssh/id_ed25519.pub";
+          init.defaultbranch = "main";
+        };
       };
     };
-  };
 
-  home.file = {
-    ".ssh".source = config.lib.file.mkOutOfStoreSymlink "${toString config.home.homeDirectory}/remote/.ssh";
-    ".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink "${toString config.home.homeDirectory}/.config/desktop/home-manager";
-    ".ssh/allowed_signers".text = lib.strings.concatStringsSep "\n" [
-      "<admin@laksith.dev> ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6zftyMUeIQVYkRag6CxWqYShjWnErQ24NeaU95Bp2z laksith@quirrel"
-      "<admin@laksith.dev> ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB1k8sWCp4/J+uw5RFHQ0UrVJpxK7fExlJlALNsHehs8 laksith@tsunami"
-    ];
+    stateVersion = "24.05"; # Please read the comment before changing.
   };
-
-  home.stateVersion = "24.05"; # Please read the comment before changing.
 }
